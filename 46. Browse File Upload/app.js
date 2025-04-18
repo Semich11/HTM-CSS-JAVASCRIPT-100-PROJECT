b6aaf78aeb21ac8938de510001899d09ba5af8ce
semich11 <chima11christopher@gmail.com>
2025-04-18 21:43:32 +0100

    Completed app.js

--- 46. Browse File Upload/app.js
+++ 46. Browse File Upload/app.js
@@ -1,24 +1,19 @@
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Browse File Upload</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <section class="wrapper">
-      <div class="icon">
-        <p class="close">&#10006;</p>
-      </div>
-      <h2 class="text">Upload a file from your computer!</h2>
-      <div class="img">
-        <img src="" />
-      </div>
-      <input type="file" id="file" />
-      <label for="file" class="upload">Browse File</label>
-    </section>
+const upload = document.querySelector("input");
+const image = document.querySelector(".img img");
+const close = document.querySelector(".icon .close");
 
-    <script src="app.js"></script>
-  </body>
-</html>
+upload.addEventListener("change", () => {
+  uploadFile(event);
+});
+
+function uploadFile(event) {
+  image.classList.add("active");
+  close.classList.add("exit");
+
+  image.src = URL.createObjectURL(event.target.files[0]);
+
+  close.addEventListener("click", () => {
+    image.classList.toggle("active");
+    close.classList.toggle("exit");
+  });
+}
